6ce0a9db16b1e02218e735b4d62c1a31b90d71ed
M-Iuliu <miuliuemanuel@gmail.com>
2024-06-03 17:50:38 +0300

    Setting up Git

---
+++ src/main/java/com/accounting/AccountingAndManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingAndManagementSystemApplication {
+
+	// First Push
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingAndManagementSystemApplication.class, args);
+	}
+
+}
