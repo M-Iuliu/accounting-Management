667ed9f6442332ca6a8ad3690b8c1572fef2edb8
andreeaRg <radulescu.andreea98@gmail.com>
2025-06-01 09:38:17 +0300

    feat: added User

---
+++ src/main/java/com/accounting/entity/User.java
@@ -0,0 +1,25 @@
+package com.accounting.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password; // Store encoded (hashed) password
+
+    private String role; // Example: "ROLE_ADMIN"
+
+}
---
+++ src/main/java/com/accounting/service/user/UserDetailsService.java
@@ -0,0 +1,9 @@
+package com.accounting.service.user;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public interface UserDetailsService {
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+}
